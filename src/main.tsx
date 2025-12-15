64aba6fdd8d7aad2c3f6311ac2a1180829934b50
Carlos Flores <117421002+CarlosFloresVaras@users.noreply.github.com>
2025-12-14 21:30:45 -0600

    Wrap App component with BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootElement = document.getElementById("root");
+
+if (!rootElement) {
+  throw new Error("Root element not found");
+}
+
+createRoot(rootElement).render(
+  <BrowserRouter basename="/classtimematcherup">
+    <App />
+  </BrowserRouter>
+);
